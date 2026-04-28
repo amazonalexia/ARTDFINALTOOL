2c024d56477b1b25e42501086e0d96761c71ef43
Rivas <acrivas@student.uiwtx.edu>
2026-04-28 15:53:51 -0500

    message

--- src/tools/KeyframeRemover.py
+++ src/tools/KeyframeRemover.py
@@ -7,6 +7,29 @@ class KeyframeController:
         controller = mc.ls(sl=True) [0]
         anim_curves = mc.listConnections(controller, t='animCurve')
         print(anim_curves)
+        
+    def isAnimationCurveFlat(animCurve):
+        if not mc.objExists():
+            raise ValueError(f"Object '{animCurve}' does not exist.")
+
+            values = mc.keyframe(anim_curve, query=True, valueChange=True)
+            if not values:
+                return True
+
+            first_val = values[0]
+            return all(abs(v - first_val) < 1e-6 for v in values)
+        
+    def deleteKeyAtCurrentTime(currentTime):
+         current_time = mc.currentTime(query=True)
+    mc.cutKey(time=(deleteKeyAtCurrentTime), clear=True)
+    print(f"Deleted keyframe(s) at frame: {deleteKeyAtCurrentTime}")
+
+    def Run():
+         keyframeRemoverWidget = KeyframeRemoverWidget()
+         keyframeRemoverWidget.show()
+            
+    Run()
+
 
 class KeyframeRemoverWidget(MayaWidget):
     def __init__(self):
@@ -23,6 +46,17 @@ class KeyframeRemoverWidget(MayaWidget):
 
         self.nameKeyframeEdit = QLineEdit()
         self.infoLayout.addWidget(self.nameKeyframeEdit)
+
+        self.removeExcessKeyframesBtn = QPushButton("Remove Excess Keyframes")
+        self.removeExcessKeyframesBtn.clicked.connect(self.removeExcessKeyframesBtnClicked)
+        self.infoLayout.addWidget(self.removeExcessKeyframesBtn)
+
+    def removeExcessKeyframesBtnClicked(self):
+                selected_objects = mc.ls(selection=True)   
+                if selected_objects:
+                    mc.cutKey(selected_objects, clear=True)
+                print("Excess Keyframes Removed!")
+
 
 def Run():
     limbRiggerWidget = KeyframeRemoverWidget()
